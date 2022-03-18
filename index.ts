ed1d6eb7b6ad8f039f94d0937d0ff7ee924310d8
Akash52 <ac8572611@gmail.com>
2022-03-18 10:19:05 +0530

    Express & we setup basic server

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+//express.json() is a middleware that parses the incoming request body and exposes it on req.body
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(5000, () => {
+  console.log(`Server started on port ${PORT} mode ${process.env.NODE_ENV}`)
+})
